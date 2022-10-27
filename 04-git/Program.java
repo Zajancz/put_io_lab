e53fc9b62396d4c27c27a95a32c57dfb77881767
student <student@lab-45-4.cs.put.poznan.pl>
2022-10-27 07:06:24 +0200

    3zmianycom

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,7 +1,16 @@
 public class Hello{
+    //comment
     public static void main(String[] args){
-      System.out.print("Hello World");
-      System.out.print("Hello World 2 ");
+      //comment
+      System.out.print("HeLLO");
+      System.out.print("World");
+      System.out.print("2");
+
+      float a;
     }
+    int x = 5;
+
   }
+
+  
 
